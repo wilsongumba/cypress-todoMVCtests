207c3e5f81f43553e02c0f8a78003c9ebdb433ed
wilson gumba <wilsongumba@gmail.com>
2021-04-05 14:17:47 +0300

    add page objects functions

--- cypress/integration/todomvc-actions.spec.js
+++ cypress/integration/todomvc-actions.spec.js
@@ -1,33 +1,40 @@
 /// <reference types="cypress" />
-import {TodoPage} from '../page-objects/todo-page'
-
-describe('todo actions', () => {
-  const todoPage = new TodoPage()
-
-  beforeEach(() => {
-    todoPage.navigate()
-
-    todoPage.addTodo('Clean room')
-  })
-
-  it('should add a new todo to the list', () => {
-    todoPage.validateTodoText(0, 'Clean room')
-
-    todoPage.validateToggleState(0, false)
-  })
-
-  describe('toggling todos', () => {
-    it('should toggle test correctly', () => {
-      todoPage.toggleTodo(0)
-      todoPage.validateTodoCompletedState(0, true)
+import {
+    navigate,
+    addTodo,
+    validateTodoText,
+    toggleTodo,
+    clearCompleted,
+    validateTodoCompletedState,
+    validateToggleState,
+    validateNumberOfTodosShown,
+  } from '../page-objects/todo-page'
+  
+  describe('todo actions', () => {
+    beforeEach(() => {
+      navigate()
+  
+      addTodo('Clean room')
     })
-
-    it('should clear completed', () => {
-      todoPage.toggleTodo(0)
-
-      todoPage.clearCompleted()
-
-      todoPage.validateNumberOfTodosShown(0)
+  
+    it('should add a new todo to the list', () => {
+      validateTodoText(0, 'Clean room')
+  
+      validateToggleState(0, false)
+    })
+  
+    describe('toggling todos', () => {
+      it('should toggle test correctly', () => {
+        toggleTodo(0)
+        validateTodoCompletedState(0, true)
+      })
+  
+      it('should clear completed', () => {
+        toggleTodo(0)
+  
+        clearCompleted()
+  
+        validateNumberOfTodosShown(0)
+      })
     })
   })
-})
--- cypress/integration/todomvc-filter.spec.js
+++ cypress/integration/todomvc-filter.spec.js
@@ -1,34 +1,41 @@
 /// <reference types="cypress" />
-import {TodoPage} from '../page-objects/todo-page'
-
-describe('filtering', function() {
-  const todoPage = new TodoPage()
-
-  beforeEach(() => {
-    todoPage.navigate()
-
-    todoPage.addTodo('Clean room')
-    todoPage.addTodo('Learn JavaScript')
-    todoPage.addTodo('Use Cypress')
-
-    todoPage.toggleTodo(1)
+import {
+    navigate,
+    addTodo,
+    toggleTodo,
+    showOnlyActiveTodos,
+    showOnlyCompletedTodos,
+    showAllTodos,
+    validateNumberOfTodosShown,
+  } from '../page-objects/todo-page'
+  
+  describe('filtering', function() {
+    beforeEach(() => {
+      navigate()
+  
+      addTodo('Clean room')
+      addTodo('Learn JavaScript')
+      addTodo('Use Cypress')
+  
+      toggleTodo(1)
+    })
+  
+    it('should filter "Active" correctly', () => {
+      showOnlyActiveTodos()
+  
+      validateNumberOfTodosShown(2)
+    })
+  
+    it('should filter "Completed" correctly', () => {
+      showOnlyCompletedTodos()
+  
+      validateNumberOfTodosShown(1)
+    })
+  
+    it('should filter "All" correctly', () => {
+      showAllTodos()
+  
+      validateNumberOfTodosShown(3)
+    })
   })
-
-  it('should filter "Active" correctly', () => {
-    todoPage.showOnlyActiveTodos()
-
-    todoPage.validateNumberOfTodosShown(2)
-  })
-
-  it('should filter "Completed" correctly', () => {
-    todoPage.showOnlyCompletedTodos()
-
-    todoPage.validateNumberOfTodosShown(1)
-  })
-
-  it('should filter "All" correctly', () => {
-    todoPage.showAllTodos()
-
-    todoPage.validateNumberOfTodosShown(3)
-  })
-})
+  
