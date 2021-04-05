4f98b4c2ffb3f1813cf85b26a6f21d6bd49b332d
wilson gumba <wilsongumba@gmail.com>
2021-04-05 15:13:09 +0300

    add visual validation

---
+++ cypress/integration/todo-visuals.spec.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+import * as todoPage from '../page-objects/todo-page'
+
+describe('visual validation', () => {
+    before(() => todoPage.navigate())
+    //before(() => cy.visit('http://todomvc-app-for-testing.surge.sh/?different-title-color'))
+
+    beforeEach(() =>
+        cy.eyesOpen({
+            appName: 'TAU TodoMVC',
+            batchName: 'TAU TodoMVC',
+            browser: [
+                { name: 'chrome', width: 1024, height: 768 },
+                { name: 'chrome', width: 800, height: 600 },
+                { name: 'firefox', width: 1024, height: 768 },
+                { deviceName: 'iPhone X' },
+            ]
+        })
+    )
+
+    afterEach(() => cy.eyesClose())
+
+    it('should look good', () => {
+        cy.eyesCheckWindow('empty todo list')
+
+        todoPage.addTodo('Clean room')
+        todoPage.addTodo('Learn javascript')
+
+        cy.eyesCheckWindow('two todos')
+
+        todoPage.toggleTodo(0)
+
+        cy.eyesCheckWindow('mark as completed')
+    })
+})
